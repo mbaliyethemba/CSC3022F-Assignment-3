a42093471dba0aebf4aa444357ea27ec8698b782
Themba <SHNMBA004@uct.ac.za>
2020-03-15 20:46:06 +0200

    Added the six big functions and other functions to the huffman node class

--- HuffmanNode.h
+++ HuffmanNode.h
@@ -14,7 +14,23 @@ namespace SHNMBA004{
 			std::shared_ptr<HuffmanNode> left;
 			//right node
 			std::shared_ptr<HuffmanNode> right;
-	}
+		
+		public:
+			HuffmanNode(); //constructor
+			~HuffmanNode(); //destructor
+			void setCharacter(char c);
+			void setFrequency(int f);
+			void setLeftChild(HuffmanNode lc);
+			void setRightChild(HuffmanNode rc);
+			char getCharacter();
+			int getFrequency();
+			HuffmanNode * getLeftChild();
+			HuffmanNode * getRightChild();
+			HuffmanNode (const HuffmanNode & rhs); //copy constructor
+			HuffmanNode (HuffmanNode && rhs); //move constructor
+			HuffmanNode & operator = (const HuffmanNode & rhs); //copy assignment
+			HuffmanNode & operatoe = (HuffmanNode && rhs); //move assignment operator
+	};
 }
 
 #endif /* HUFFMANNODE_H_ */
