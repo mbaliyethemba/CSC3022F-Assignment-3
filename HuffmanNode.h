003d17128ea8af8cbd3f2547fd5eea6ad3117b79
Themba <SHNMBA004@uct.ac.za>
2020-03-15 20:37:52 +0200

    Added the private variables for the huffman node class

--- HuffmanNode.h
+++ HuffmanNode.h
@@ -4,6 +4,17 @@
 #include <memory>
 
 namespace SHNMBA004{
+	class HuffmanNode{
+		private:
+			//stored in node
+			char character; 
+			//frequency of occurence
+			int freq;
+			//left node
+			std::shared_ptr<HuffmanNode> left;
+			//right node
+			std::shared_ptr<HuffmanNode> right;
+	}
 }
 
 #endif /* HUFFMANNODE_H_ */
