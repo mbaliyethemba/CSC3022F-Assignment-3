d7641b439f4506de30eedfdadcfe59b989c6f082
Themba <SHNMBA004@uct.ac.za>
2020-03-15 21:00:01 +0200

    Added the node constructor

--- HuffmanNode.cpp
+++ HuffmanNode.cpp
@@ -5,3 +5,13 @@
 
 #include "HuffmanNode.h"
 #include <memory>
+
+namespace SHNMBA004{
+	//Node Constructor
+	HuffmanNode::HuffmanNode(){
+		character = 0;
+		frequency = 0;
+		std::shared_ptr<HuffmanNode> left = nullptr;
+		std::shared_ptr<HuffmanNode> right = nullptr;
+	}
+}
