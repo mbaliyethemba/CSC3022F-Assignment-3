ce738adad81c100035407751260f44c7a9261707
Themba <SHNMBA004@uct.ac.za>
2020-03-15 22:35:30 +0200

    Added the driver class

---
+++ driver.cpp
@@ -0,0 +1,10 @@
+//Student Name: Mbaliyethemba Shangase
+//Student Number: SHNMBA004
+//Due Date: 16/03/2020
+//Assignment 3
+
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include "HuffmanTree.h"
