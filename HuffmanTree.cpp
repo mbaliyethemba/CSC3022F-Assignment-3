74019e43bd0dc03036a67ccca63efabd0180ae16
Themba <SHNMBA004@uct.ac.za>
2020-03-15 21:36:56 +0200

    Added the Huffman tree class

---
+++ HuffmanTree.cpp
@@ -0,0 +1,10 @@
+//Student Name: Mbaliyethemba Shangase
+//Student Number: SHNMBA004
+//Due Date: 16/03/2020
+//Assignment 3
+
+#include "HuffmanTree.h"
+#include "HuffmanNode.h"
+#include <queue>
+#include <iomanip>
+#include <fstream>
